391695808e2f584410bde01cbe1d22f5b40fd67e
trieutrng <trieutrng@gmail.com>
2026-05-31 23:47:56 +0700

    playable dungeon

---
+++ src/systems/map_render.rs
@@ -0,0 +1,52 @@
+use crate::prelude::*;
+
+#[system]
+#[read_component(FieldOfView)]
+#[read_component(Player)]
+pub fn map_render(
+    ecs: &SubWorld,
+    #[resource] map: &Map, 
+    #[resource] camera: &Camera
+) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+    let mut draw_batch = DrawBatch::new();
+    draw_batch.target(0);
+    for y in camera.top_y ..= camera.bottom_y {
+        for x in camera.left_x .. camera.right_x {
+            let pt = Point::new(x, y);
+            let offset = Point::new(camera.left_x, camera.top_y);
+            let idx: usize = map_idx(x, y);
+            if map.in_bounds(pt) && (player_fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx]) {
+                let tint = if player_fov.visible_tiles.contains(&pt) {
+                    WHITE
+                } else {
+                    DARK_GRAY
+                };
+                match map.tiles[idx] {
+                    TileType::Floor => {
+                        draw_batch.set(
+                            pt - offset,
+                            ColorPair::new(
+                                tint,
+                                BLACK
+                            ),
+                            to_cp437('.')
+                        );
+                    },
+                    TileType::Wall => {
+                        draw_batch.set(
+                            pt - offset,
+                            ColorPair::new(
+                                tint,
+                                BLACK
+                            ),
+                            to_cp437('#')
+                        );
+                    }
+                }
+            }
+        }
+    }
+    draw_batch.submit(0).expect("Batch error");
+}
